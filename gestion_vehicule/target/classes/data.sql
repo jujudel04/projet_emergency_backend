6f231eee297fd83e3b44aebfb7d046092ba7fd2e
jujudel04 <78476197+jujudel04@users.noreply.github.com>
2021-06-04 14:35:14 +0200

    gestion vehicule ok

--- gestion_vehicule/target/classes/data.sql
+++ gestion_vehicule/target/classes/data.sql
@@ -2,4 +2,4 @@ insert into vehicle(id,lon,lat,type,efficiency,
 			liquid_type,liquid_quantity,liquid_consumption,fuel,
 			fuel_consumption,crew_member, crew_member_capacity,facility_ref_id)values(1,0,0,'CAR',10,'WATER',50,10,50,10,1,10,1);
 
-insert into caserne(id,vehicules,lon,lat)values(1,'1:2:3:4',0,0);
+insert into caserne(id,lon,lat)values(1,0,0);
